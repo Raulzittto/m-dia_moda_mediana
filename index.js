d793acf7b7df13efabb74b4537fb1d814fd6ca9a
Raul <soaresraul22@gmail.com>
2024-06-24 10:39:56 -0300

    add: adicionado a lógica de moda

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 console.log("Insira valores para calcular sua média, moda e mediana.");
 const conjuntoNumeros = [1, 4, 10, 17, 1, 1];
-
-process.stdin.on("data", function (data) {
-  let soma = 0;
-  for (var i = 0; i < conjuntoNumeros.length; i++) {
-    soma += conjuntoNumeros[i];
-    /*if (conjuntoNumeros.includes(conjuntoNumeros[i])) {
-            i += 1;
-            var moda = numero[i];
-          }*/
-
-    if (conjuntoNumeros.length % 2 == 0) {
-      var b = 0;
-      b = conjuntoNumeros.length;
-      b = b / 2;
-      b = conjuntoNumeros[b] + conjuntoNumeros[b - 1];
-
-      b = b / 2;
-      mediana = b;
-    } else {
-      b = 0;
-      b = conjuntoNumeros.length;
-      b = b - 1;
-      b = b / 2;
-      var mediana = conjuntoNumeros[b];
-    }
+let repeticao = {};
+let soma = 0;
+for (i = 0; i < conjuntoNumeros.length; i++) {
+  if (repeticao[conjuntoNumeros[i]]) {
+    repeticao[conjuntoNumeros[i]]++;
+  } else {
+    repeticao[conjuntoNumeros[i]] = 1;
   }
+}
+let array = Object.entries(repeticao);
+let moda;
+let quantidadeDeAparicoes = 0;
+for (i = 0; i < array.length; i++) {
+  if (array[i][1] > quantidadeDeAparicoes) {
+    quantidadeDeAparicoes = array[i][1];
+    moda = array[i][0];
+  }
+}
+for (var i = 0; i < conjuntoNumeros.length; i++) {
+  soma += conjuntoNumeros[i];
 
-  console.log("A média será: " + soma / conjuntoNumeros.length);
-  //console.log("A moda será: " + moda);
-  console.log("A mediana será: " + mediana);
-});
+  if (conjuntoNumeros.length % 2 == 0) {
+    var b = 0;
+    b = conjuntoNumeros.length;
+    b = b / 2;
+    b = conjuntoNumeros[b] + conjuntoNumeros[b - 1];
+
+    b = b / 2;
+    mediana = b;
+  } else {
+    b = 0;
+    b = conjuntoNumeros.length;
+    b = b - 1;
+    b = b / 2;
+    var mediana = conjuntoNumeros[b];
+  }
+}
+
+console.log("A média será: " + soma / conjuntoNumeros.length);
+console.log("A moda será: " + moda);
+console.log("A mediana será: " + mediana);
